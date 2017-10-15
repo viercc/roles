11aa43e0680b320a289bcc329c3ba33572961fa4
Matt Noonan <matt.noonan@gmail.com>
2017-10-15 00:54:22 -0400

    For now, remove Wrapped and related instances.

--- src/Data/Roles.hs
+++ src/Data/Roles.hs
@@ -7,36 +7,17 @@
 module Data.Roles
   ( Representational(rep)
   , Phantom(phantom)
-  , new, eta
   ) where
 
 import Control.Applicative
-import Control.Concurrent.STM
-import Control.Lens hiding (coerce)
-import "mtl" Control.Monad.Reader
 import Control.Monad.ST
-import "mtl" Control.Monad.State
-import "mtl" Control.Monad.Writer
 import Data.Complex
-import Data.Functor.Compose
 import Data.Monoid
 import Data.Proxy
-import Data.Tagged
 import Data.Type.Coercion
-import GHC.Prim (Coercible, coerce)
+import Data.Coerce
 import Unsafe.Coerce
-
-coerce1 :: Coercible a b => Coercion a c -> Coercion b c
-coerce1 = coerce
-
-coerce2 :: Coercible b c => Coercion a b -> Coercion a c
-coerce2 = coerce
-
-new :: (Rewrapping s t, Coercible (Unwrapped s) s, Coercible (Unwrapped t) t) => Coercion (Unwrapped s) (Unwrapped t) -> Coercion s t
-new = coerce1 . coerce2
-
-eta :: forall (f :: x -> y) (g :: x -> y) (a :: x). Coercion f g -> Coercion (f a) (g a)
-eta = unsafeCoerce
+import Data.Map
 
 class Representational (t :: k1 -> k2) where
   -- | An argument is representational if you can lift a coercion of the argument into one of the whole
@@ -44,7 +25,7 @@ class Representational (t :: k1 -> k2) where
   default rep :: Phantom t => Coercion a b -> Coercion (t a) (t b)
   rep _ = phantom
 
-class Representational t => Phantom t where
+class Representational t => Phantom (t :: k1 -> k2) where
   -- | An argument is phantom if you can 'coerce' the whole ignoring the argument
   phantom :: Coercion (t a) (t b)
   default phantom :: Coercible (t a) (t b) => Coercion (t a) (t b)
@@ -54,12 +35,6 @@ class Representational t => Phantom t where
 
 instance Representational Proxy
 instance Phantom Proxy
-
--- * Data.Tagged
-
-instance Representational Tagged
-instance Phantom Tagged
-instance Representational (Tagged a) where rep Coercion = Coercion
 
 -- * Const
 
@@ -109,7 +84,9 @@ instance Representational []      where rep Coercion = Coercion
 instance Representational Maybe   where rep Coercion = Coercion
 instance Representational IO      where rep Coercion = Coercion
 instance Representational (ST s)  where rep Coercion = Coercion
-instance Representational STM     where rep Coercion = Coercion
+
+-- * Data.Map
+instance Representational (Map k) where rep Coercion = Coercion
 
 -- * Data.Complex
 
@@ -124,40 +101,4 @@ instance Representational Endo    where rep Coercion = Coercion
 instance Representational First   where rep Coercion = Coercion
 instance Representational Last    where rep Coercion = Coercion
 
--- * WrappedMonad
 
-instance Representational WrappedMonad where
-  rep Coercion = Coercion
-
-instance Representational m => Representational (WrappedMonad m) where
-  rep = new.rep
-
--- * WrappedArrow
-
-instance Representational WrappedArrow where
-  rep Coercion = Coercion
-
-instance Representational p => Representational (WrappedArrow p) where
-  rep = unsafeCoerce
-
-instance Representational (p a) => Representational (WrappedArrow p a) where
-  rep = unsafeCoerce
-
--- * Compose
-
-instance Representational f => Representational (Compose f) where
-  rep = unsafeCoerce
-
-instance (Representational f, Representational g) => Representational (Compose f g) where
-  rep = new.rep.rep
-
--- * MTL
-
-instance Representational m => Representational (StateT s m) where
-  rep = new.rep.rep.eta.rep
-
-instance Representational m => Representational (ReaderT e m) where
-  rep = new.rep.rep
-
-instance Representational m => Representational (WriterT w m) where
-  rep = new.rep.eta.rep

d46fbef8d3c623bbde7558d78784840265d1e3fd
Koji Miyazato <viercc@gmail.com>
2020-04-03 17:58:54 +0900

    Add instances, remove unused extension

--- src/Data/Roles.hs
+++ src/Data/Roles.hs
@@ -1,24 +1,29 @@
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE Rank2Types #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE PackageImports #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE DefaultSignatures   #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE InstanceSigs        #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Data.Roles
   ( Representational(rep)
   , Phantom(phantom)
   ) where
 
-import Control.Applicative
-import Control.Monad.ST
-import Data.Complex
-import Data.Monoid
-import Data.Proxy
-import Data.Type.Coercion
-import Data.Coerce
-import Unsafe.Coerce
-import Data.Map
-import Data.IntMap
+import           Control.Applicative
+import           Control.Monad.ST (ST)
+
+import           Data.Complex
+import           Data.Monoid
+import           Data.Semigroup
+
+import           Data.Coerce
+import           Data.IntMap
+import           Data.Map
+
+import           Data.Proxy
+import           Data.Type.Coercion
+import           Unsafe.Coerce
 
 class Representational (t :: k1 -> k2) where
   -- | An argument is representational if you can lift a coercion of the argument into one of the whole
@@ -86,21 +91,35 @@ instance Representational Maybe   where rep Coercion = Coercion
 instance Representational IO      where rep Coercion = Coercion
 instance Representational (ST s)  where rep Coercion = Coercion
 
--- * containers
-instance Representational (Map k) where rep Coercion = Coercion
-instance Representational IntMap  where rep Coercion = Coercion
-
 -- * Data.Complex
 
 instance Representational Complex where rep Coercion = Coercion
 
--- * Data.Monoid
+-- * Data.Semigroup
 
-instance Representational Sum     where rep Coercion = Coercion
+instance Representational Arg     where rep Coercion = Coercion
+instance Representational (Arg a) where rep Coercion = Coercion
 instance Representational Product where rep Coercion = Coercion
+instance Representational Sum     where rep Coercion = Coercion
 instance Representational Dual    where rep Coercion = Coercion
 instance Representational Endo    where rep Coercion = Coercion
-instance Representational First   where rep Coercion = Coercion
-instance Representational Last    where rep Coercion = Coercion
+instance Representational Option  where rep Coercion = Coercion
 
+instance Representational Data.Semigroup.First where rep Coercion = Coercion
+instance Representational Data.Semigroup.Last  where rep Coercion = Coercion
+instance Representational WrappedMonoid        where rep Coercion = Coercion
 
+-- * Data.Monoid
+
+instance Representational Ap                  where rep Coercion = Coercion
+instance Representational f => Representational (Ap f) where
+  rep :: forall a b. Coercion a b -> Coercion (Ap f a) (Ap f b)
+  rep c = case rep c :: Coercion (f a) (f b) of
+    Coercion -> Coercion
+
+instance Representational Data.Monoid.First   where rep Coercion = Coercion
+instance Representational Data.Monoid.Last    where rep Coercion = Coercion
+
+-- * containers
+instance Representational (Map k) where rep Coercion = Coercion
+instance Representational IntMap  where rep Coercion = Coercion
